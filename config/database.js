a97a824a0ce0ec5b8df190c459d51ddb8f6efbce
Adrian Schmitz <scegad@scegad.io>
2018-02-16 18:22:31 -0500

    heroku adjustments

--- config/database.js
+++ config/database.js
@@ -2,14 +2,8 @@ import mongoose from 'mongoose';
 
 // Database configuration
 
-if (process.env.NODE_ENV != "production") {
-  const MONGODB_URI = `mongodb://${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}`;
-} else {
-  const MONGODB_URI = process.env.MONGODB_URI;
-}
-
 // Set Mongoose to use ES6 Promise API, and connect to DB
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI);
 
 export default dbConfig;
